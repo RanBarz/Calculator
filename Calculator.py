3970846ddb1edf5ac58196c68ce1c2a5caccb282
Ran <ranbarzelay13@gmail.com>
2024-12-06 12:21:38 +0200

    feat: added a a function that makes a binary tree from the tokens of the mathematical expression, and functions that turn a string to tokens and evaluate the tree.\n The functions work only for expressions with single digit numbers, no parenthesis and simple operators.

--- Calculator.py
+++ Calculator.py
@@ -1,8 +1,15 @@
-PRECEDENCE_DICTIONARY = {"+": 1, "-": 1, "*": 2, "/": 2, "^": 3, "%": 4, "@": 5, "$": 5, "&": 5, "~": 6, "!": 6}
+from StateOfToken import StateOfToken
+from TreeNode import TreeNode
+from Number import Number
+
+PRECEDENCE = {"+": 1, "-": 1, "*": 2, "/": 2, "^": 3, "%": 4, "@": 5, "$": 5, "&": 5, "~": 6, "!": 6}
+OPERATORS = {"+": Number.__add__, "-": Number.__sub__, "*": Number.__mul__, "/": Number.__truediv__, "^": Number.__pow__
+    , "%": Number.__mod__, "@": Number.__matmul__, "$": Number.maximum, "&": Number.__and__
+    , "~": Number.__ne__, "!": Number.factorial}
 
 class Calculator:
-    def __init__(self, string):
-        self.__string = string
+    def __init__(self, expression):
+        self.__expression = expression
 
     def parse_expression(self):
         """
@@ -10,6 +17,9 @@ class Calculator:
          so it can be evaluated efficiently later.
         :return: this function will return the abstract data type representation of the string.
         """
+        tokens = self.get_tokens()
+        parsed_expression = Calculator.make_tree_of_tokens(tokens)
+        return parsed_expression
 
     def validate_expression(self):
         """
@@ -22,9 +32,62 @@ class Calculator:
         A function that will remove all white spaces in an expression, as they are not significant
         """
 
+    def get_tokens(self):
+        lst = []
+        for token in self.__expression:
+            if Number.is_number(token):
+                lst.append(Number.from_string(token))
+            else:
+                lst.append(token)
+        return lst
+
     @staticmethod
-    def evaluate_expression():
+    def make_tree_of_tokens(tokens):
+        tree = TreeNode()
+        head = tree
+        state = StateOfToken.FIRST_OPERAND
+        for token in tokens:
+            if state == StateOfToken.FIRST_OPERAND:
+                left = TreeNode(token)
+                tree.set_left(left)
+                state = StateOfToken.OPERATOR
+            elif state == StateOfToken.SECOND_OPERAND:
+                right = TreeNode(token)
+                tree.set_right(right)
+                state = StateOfToken.OPERATOR
+            else:
+                if tree.get_value() == 0:
+                    tree.set_value(token)
+                elif PRECEDENCE[tree.get_value()] >= PRECEDENCE[token]:
+                    new_tree = TreeNode(token)
+                    new_tree.set_left(head)
+                    tree = new_tree
+                    head = tree
+                else:
+                    sub_tree = TreeNode(token)
+                    sub_tree.set_left(tree.get_right())
+                    tree.set_right(sub_tree)
+                    tree = sub_tree
+                state = StateOfToken.SECOND_OPERAND
+        return head
+
+    @staticmethod
+    def evaluate_expression(parsed_expression):
         """
         A method that takes the abstract data type representation of the expression and evaluates it
         :return: the method will return the value.
         """
+        left = parsed_expression.get_left()
+        right = parsed_expression.get_right()
+        if Calculator.is_operator(left.get_value()):
+            left.set_value(Calculator.evaluate_expression(left))
+        if Calculator.is_operator(right.get_value()):
+            right.set_value(Calculator.evaluate_expression(right))
+        left_operand = left.get_value()
+        right_operand = right.get_value()
+        operator = parsed_expression.get_value()
+        return OPERATORS[operator](left_operand, right_operand)
+
+    @staticmethod
+    def is_operator(char):
+        return char in OPERATORS
--- Number.py
+++ Number.py
@@ -1,11 +1,88 @@
 class Number:
-    @staticmethod
-    def get_number(string):
-        """
-        A method that receives a string representation of a number and returns its value
-        :param string: representation of a number
-        """
 
+    def __init__(self, number):
+        self.__number = number
+
+    @classmethod
+    def from_string(cls, string_of_number):
+         return cls(Number.get_number(string_of_number))
+
+    def get_value(self):
+        return self.__number
+
+    @staticmethod
+    def __mul__(self, other):
+        """Returns the product of two numbers."""
+        return Number(self.__number * other.get_value())
+
+
+    @staticmethod
+    def __truediv__(self, other):
+        """Returns the division of two numbers, raises an error if dividing by zero."""
+        if other.get_value() == 0:
+            raise ZeroDivisionError("Cannot divide by zero.")
+        return Number(self.__number / other.get_value())
+
+    @staticmethod
+    def __add__(self, other):
+        """Returns the sum of two numbers."""
+        return Number(self.__number + other.get_value())
+
+    @staticmethod
+    def __sub__(self, other):
+        """Returns the difference between two numbers."""
+        return Number(self.__number - other.get_value())
+
+    @staticmethod
+    def __pow__(self, other):
+        """Returns num1 raised to the power of num2."""
+        return Number(pow(self.__number, other.get_value()))
+
+    @staticmethod
+    def __matmul__(self, other):
+        """Returns the average of two numbers."""
+        return Number((self.__number + other.get_value()) / 2)
+
+    @staticmethod
+    def maximum(self, other):
+        """Returns the maximum of two numbers."""
+        return Number(max(self.__number, other.get_value()))
+
+    @staticmethod
+    def __and__(self, other):
+        """Returns the minimum of two numbers."""
+        return Number(min(self.__number, other.get_value()))
+
+    @staticmethod
+    def __mod__(self, other):
+        """Returns the modulo of two numbers, raises an error if dividing by zero."""
+        if other.get_value() == 0:
+            raise ZeroDivisionError("Cannot perform modulo by zero.")
+        return Number(self.__number % other.get_value())
+
+    @staticmethod
+    def __invert__(self):
+        """Returns the negated value of the number."""
+        return Number(-self.__number)
+
+    @staticmethod
+    def factorial(self):
+        """Returns the factorial of a number, handles edge cases and uses an iterative approach."""
+        if self.__number < 0:
+            raise ValueError("Factorial is not defined for negative numbers.")
+        if self.__number == 1 or self.__number == 0:
+            return 1
+        return self.__number * Number.factorial(self.__number - 1)
+
+
+    @staticmethod
+    def is_number(string):
+        """
+        A method that determines if a string represents a number
+        :param string:
+        :return: True if the string can be a number, False otherwise
+        """
+        return string.isdigit()
     @staticmethod
     def is_integer(string):
         """
@@ -21,3 +98,11 @@ class Number:
     @staticmethod
     def get_float(string):
         """ A method that get a string representation of a float and returns its value """
+
+    @staticmethod
+    def get_number(string):
+        """
+        A method that receives a string representation of a number and returns its value
+        :param string: representation of a number
+        """
+        return int(string)
