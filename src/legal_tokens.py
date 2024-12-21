32d3a9e4accbf0c66822b364157055acbef2c5d0
RanBarz <162011958+RanBarz@users.noreply.github.com>
2024-12-21 15:55:25 +0200

    hotFix: made the code follow PEP8 rules more strictly.

--- src/legal_tokens.py
+++ src/legal_tokens.py
@@ -1,7 +1,11 @@
+"""A module for an enum of non-numbers and non-operators,
+ that are legal in an expression."""
 from enum import Enum
 
 
 class LegalTokens(Enum):
+    """An enum of legal tokens."""
+
     FLOATING_POINT = '.'
     CLOSING_PARENTHESIS = ')'
     OPENING_PARENTHESIS = '('
