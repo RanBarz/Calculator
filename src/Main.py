38e65df5c326cb28d38a1858b933d838162484e2
RanBarz <162011958+RanBarz@users.noreply.github.com>
2024-12-21 15:55:44 +0200

    hotFix: made the code follow PEP8 rules more strictly.

--- src/Main.py
+++
@@ -1,9 +0,0 @@
-from UserInterface import UserInterface
-
-
-def main():
-    UserInterface.open_menu()
-
-
-if __name__ == "__main__":
-    main()
---
+++ src/main.py
@@ -0,0 +1,11 @@
+"""A module that begins the use of the calculator."""
+from user_interface import UserInterface
+
+
+def main():
+    """A function to open the calculator menu."""
+    UserInterface.open_menu()
+
+
+if __name__ == "__main__":
+    main()
